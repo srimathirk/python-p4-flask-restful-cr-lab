6af3cf80509879f0e3da60c7e00a801aae4f97f7
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-09-09 17:07:11 -0400

    RESTful API lab done

--- server/app.py
+++ server/app.py
@@ -15,12 +15,62 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+class Home(Resource):
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the plants RESTful API "
+        }
+        response = make_response(
+            response_dict,
+            200,
+        )
+        return response
+api.add_resource(Home,'/')
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+        return response
+
+    def post(self):
+        # new_plant = Plant(
+        #     name=request.form['name'],
+        #     image=request.form['image'],
+        #     price=request.form['price'],
+        # )
+        new_plant = Plant(
+            name=request.get_json()['name'],
+            image=request.get_json()['image'],
+            price=request.get_json()['price'],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response_dict = new_plant.to_dict()
+        response = make_response(
+            response_dict,
+            201,
+        )
+        return response
+
+api.add_resource(Plants, '/plants')
+    
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        response_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+ 
         
 
 if __name__ == '__main__':
